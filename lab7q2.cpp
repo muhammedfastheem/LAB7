b6486f960cf8e6085bec52a316ff636f2188992a
muhammedfastheem <42205163+muhammedfastheem@users.noreply.github.com>
2018-10-12 20:11:42 +0530

    Update lab7q2.cpp

--- lab7q2.cpp
+++ lab7q2.cpp
@@ -1,32 +1,29 @@
-//to print the first n natural numbers  that entered by user
+//to print the first n natural numbers
+//including library
 #include<iostream>
-//include standard library
 using namespace std;
-//return something function prototype
-int num(int);
-//start function
-int main()
-   {
-	   //declare varibles
+//starting function
+	int num(int);
+	int main()
+	{//declaring varibles
 	   int n,print;
-	   //print
-    cout<<"enter the value of n ";
-    //store input in varibles
-    cin>>n;
-    //function calling
-    print=num(n);
-    //print
-    cout<<print<<endl;
+	   //printing
+    	cout<<"enter the value of n ";
+    	//store input in varibles
+    	cin>>n;
+    	//function calling
+    	print=num(n);
+    	//printing
+    	cout<<print<<endl;
     return 0;
-   }
-   
+}
    //define recursive function
    int num(int n){
-	   //add base condition
+	   //adding base condition
 	   if(n>1){
 	   cout<<" "<<n<<" ";
        return num(n-1);
-   }
+}
        else if(n==1)
        return 1;
        else 
