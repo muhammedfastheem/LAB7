be44bbef35ea8b71685fa4845a9535f1ba3b48b4
muhammedfastheem <42205163+muhammedfastheem@users.noreply.github.com>
2018-10-12 20:25:16 +0530

    Update lab7q6.cpp

--- lab7q6.cpp
+++ lab7q6.cpp
@@ -1,28 +1,27 @@
-//to print a number in reverse order
+//printing a number in reverse order
+//including library
 #include<iostream>
-//include standard library
 using namespace std;
 //return something function prototype
-int reverse(int);
-//main function
-int main()
-   {
+	int reverse(int);
+	//main function
+	int main()
+   	{
 	   //declare varibles
 	   int p;
 	   cout<<"enter the value of p ";
 	   cin>>p;
 	   //function calling
 	   reverse(p);
-	   return 0;
+	return 0;
    }
    
   int reverse(int p){
-     if (p!=0){
-		 cout<<p%10;
-     return reverse(p/10);
-	 }
-    else
+     	if (p!=0){
+	cout<<p%10;
+     	return reverse(p/10);
+	}
+    	else
     return 0;
- 
-  }
+ }
        
