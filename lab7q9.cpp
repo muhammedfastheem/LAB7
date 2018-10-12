2e84603fd838e873365269d267f5a6bda82eb05a
muhammedfastheem <42205163+muhammedfastheem@users.noreply.github.com>
2018-10-12 20:32:08 +0530

    Update lab7q9.cpp

--- lab7q9.cpp
+++ lab7q9.cpp
@@ -1,11 +1,12 @@
 //to find the factorial of entered number
+//inluding library
 #include<iostream>
 using namespace std;
 //return something function prototype
-long factorial(int);
-//start function
-int main()
-   {
+	long factorial(int);
+	//start function
+	int main()
+   	{
 	   //declare varibles
 	   int p;
 	   cout<<"enter the value of p ";
@@ -14,13 +15,12 @@ int main()
 	   factorial(p);
 	   cout<<"factorial of no "<<p<<" is "<<factorial(p)<<endl;
 	   return 0;
-   }
+}
    
   long factorial(int p){
-     if (p==0)
-	 return 1;
-	 else	 
+	  if (p==0)
+	  return 1;
+	  else	 
      return (p*factorial(p-1));
- 
   }
        
