690fc991930601595dbb1b1ada5014cbd18100a3
muhammedfastheem <42205163+muhammedfastheem@users.noreply.github.com>
2018-10-12 20:36:35 +0530

    Update lab7q11.cpp

--- lab7q11.cpp
+++ lab7q11.cpp
@@ -1,11 +1,12 @@
 //to find highest common factor two number
+//including library
 #include<iostream>
 using namespace std;
 //return something function prototype
-int gcd(int,int);
-//main function
-int main()
-   {
+	int gcd(int,int);
+	//main function
+	int main()
+ 	{
 	   //declare varibles
 	   int p,q;
 	   cout<<"enter the value of p and q ";
@@ -13,23 +14,23 @@ int main()
 	   //function calling
 	   gcd(p,q);
 	   cout<<"highest common factor of no "<<p<<" "<<q<<" is "<<gcd(p,q)<<endl;
-	   return 0;
-   }
+	return 0;
+}
    //define function
-  int gcd(int p,int q){
-	   //base condition
-	    if(p>q){
-       if(q==0)
-       return p;
-       else
-        return gcd(q,p%q);
-	}
-      if(q>p){
-      if(p==0)
-      return q;
-      else
+   int gcd(int p,int q){
+	//base condition
+	if(p>q){
+        if(q==0)
+        return p;
+        else
+      return gcd(q,p%q);
+}
+      	if(q>p){
+      	if(p==0)
+      	return q;
+      	else
       return gcd(q%p,p);
-      }
+	}
        else
        return p;
 }
