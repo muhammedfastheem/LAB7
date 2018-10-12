ce14b7fa37c030638d1fde2592142d3427458b56
muhammedfastheem <42205163+muhammedfastheem@users.noreply.github.com>
2018-10-12 20:34:18 +0530

    Update lab7q10.cpp

--- lab7q10.cpp
+++ lab7q10.cpp
@@ -1,11 +1,12 @@
 //to print the n term of a fibonacci sequence
+//including library
 #include<iostream>
 using namespace std;
 //return something function prototype
-int fibonacci(int);
-//start function
-int main()
-   { 
+	int fibonacci(int);
+	//start function
+	int main()
+	{ 
 	   int n;
 	   //declare varibles
 	   int first,second;
@@ -14,15 +15,14 @@ int main()
 	   //function calling
 	   fibonacci(n);
 	   cout<<"the "<<n<<"th term of fibonacci sequence is "<<fibonacci(n)<<endl; 
-	   return 0;
-   }
+	return 0;
+}
    
   int fibonacci(int n){
 	  
-     if (n==1 || n==2)
-	 return 1;
-	 else	 
+ 	if (n==1 || n==2)
+	return 1;
+	else	 
      return (fibonacci(n-1)+fibonacci(n-2));
- 
-  }
+}
        
