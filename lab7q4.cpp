36d925fe28e4eebbf7ec20cfd7c60bf3950c749f
muhammedfastheem <42205163+muhammedfastheem@users.noreply.github.com>
2018-10-12 20:20:40 +0530

    Update lab7q4.cpp

--- lab7q4.cpp
+++ lab7q4.cpp
@@ -1,33 +1,33 @@
-//to print all the even or odd natural number within the entered range  
+//to print all the even or odd natural number within the entered range
+//including library
 #include<iostream>
-//include standard library
 using namespace std;
 //return something function prototype
-int num(int,int);
-//start function
-int main()
-   {
-	   //declare varibles
-	   int llimit,ulimit,print;
-	   //print
-    cout<<"enter the value of lower and upper limit ";
-    //store input in varibles
-    cin>>llimit>>ulimit;
-    //function calling
-    num(llimit,ulimit);
+	int num(int,int);
+	//start function
+	int main()
+        {
+	//declare varibles
+	int llimit,ulimit,print;
+	//print
+   		cout<<"enter the value of lower and upper limit ";
+    		//store input in varibles
+    		cin>>llimit>>ulimit;
+    		//function calling
+    		num(llimit,ulimit);
     return 0;
-   }
+}
    
    //define recursive function
    int num(int llimit,int ulimit)
-   {
+      {
 	   
 	   //add base condition
 	     if(llimit<=ulimit)
       {
 	   cout<<" "<<llimit<<" ";
        return num(llimit+2,ulimit);
-                     }
+       }
      else
      return 0;
         
